a2c3c6119418e7249b398ab57f869d961456e8d7
SD91PL <sd91pl@gmail.com>
2023-07-14 21:19:32 +0200

    enhanced burger button - x.svg added to close burger-menu

--- src/js/navbarToggler.js
+++ src/js/navbarToggler.js
@@ -4,6 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	const toggleMenu = () => {
 		burgerMenu.classList.toggle('menu-appear')
+
+		if (burgerMenu.classList.contains('menu-appear')) {
+			burgerBtn.innerHTML = '<img src="./dist/img/x.svg" alt="Przycisk do zwijania nawigacji">'
+		} else {
+			burgerBtn.innerHTML = '<img src="./dist/img/list.svg" alt="Przycisk do rozwijania nawigacji">'
+		}
 	}
 
 	burgerBtn.addEventListener('click', toggleMenu)
